28ca4f48e9b0389637718868fd3ea9f597338f19
Jorge Angel <jorgerodolfoangel@gmail.com>
2026-09-03 17:44:15 -0300

    se crea autenticacion del contexto en UserContext

--- src/context/UserContext.jsx
+++ src/context/UserContext.jsx
@@ -1,17 +1,51 @@
+import { createContext,  useState, useEffect } from 'react';
+import { usuarios } from '../data/usuarios';
 
-
-import { createContext,  useState } from 'react';
 
 export const UserContext = createContext();
 
 
 export const UserProvider=({children})=>{
-    const[user, setUser]= useState(null);
+    const[user, setUser]= useState(()=>{
+    const guardarUser= localStorage.getItem('user');
+    return guardarUser ? JSON.parse(guardarUser):null;
+});
 
+useEffect(() => {
+  if (user){
+    localStorage.setItem('user',JSON.stringify(user));
+  }
+   else  {
+    localStorage.removeItem('user');
+  }
+},[user]);
+
+const login = (email, password)=> {
+const   usuarioEcontrado= usuarios.find((usuario)=>  usuario.email === email && usuario.password === password
+);
+if (usuarioEcontrado){
+const userData ={
+    id: usuarioEcontrado.id,
+    usuario: usuarioEcontrado.usuario,
+    email: usuarioEcontrado.email,
+    rol: usuarioEcontrado.rol,
+};
+setUser(userData);
+    return { success: true };
+}
+else {
+    return { success: false, message: 'Credenciales incorrectas' };
+}
+}
+
+
+const logout = ()=>{
+    setUser(null);
+}
 
 return(
 
-<UserContext.Provider value={{user}}>
+<UserContext.Provider value={{user, login, logout}}>
 {children}
 
 </UserContext.Provider>
