84c8227b3297cf9fe1c7b166a07ffcc0128ca94f
Jorge Angel <jorgerodolfoangel@gmail.com>
2026-09-09 15:17:53 -0300

    feat: agrego BrowserRouter y vista NotFound

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,30 @@
 import './App.css';
-import { UserProvider } from './context/UserContext';
-import { Login } from './pages/Login';
-import Home from './pages/Home';
 import { BrowserRouter, Routes, Route } from 'react-router';
 import { ProtectedRoute } from './routes/ProtectedRoute';
+import { UserProvider } from './context/UserContext';
+import Login  from './pages/Login';
+import Home from './pages/Home';
+import NotFound from './pages/NotFound';
+import Admin from './pages/Admin';
+
 
 function App() {
   return (
-    <UserProvider>
-      <Routes>
-        <Route path="/login" element={<Login />} />
-        <Route path="/" element={<Home />} />
+    <BrowserRouter>
+      <UserProvider>
+        <Routes>
+          <Route path="/login" element={<Login />} />
+          <Route path="/" element={<Home />} />
 
-        {/* Ruta protegida */}
-        <Route element={<ProtectedRoute adminOnly={true} />}>
-          <Route path="/admin" element={<h1>Panel de Administración (Vista previa)</h1>} />
-        </Route>
-      </Routes>
-    </UserProvider>
+          {/* Ruta protegida */}
+          <Route element={<ProtectedRoute adminOnly={true} />}>
+            <Route path="/admin" element={<Admin />} />
+          </Route>
+
+          <Route path="*" element={<NotFound />} />
+        </Routes>
+      </UserProvider>
+    </BrowserRouter>
   );
 }
 
