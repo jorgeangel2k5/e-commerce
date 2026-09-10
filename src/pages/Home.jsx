72a4f2b0f1391419468486bce6585b8a7d6d2ee6
Alvaro Medina <168005749+alvaroMedina777@users.noreply.github.com>
2026-09-10 20:25:02 -0300

    termino product card

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,8 +1,19 @@
 import React from 'react'
+import ProductCard from '../components/ProductCard'
+import initialProducts from '../data/productos'
 
 const Home = () => {
   return (
-    <div>Home</div>
+    <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
+  {
+    initialProducts.map((producto) => (
+      <ProductCard
+        key={producto.id}
+        producto={producto}
+      />
+    ))
+  }
+</div>
   )
 }
 
