0adf6fd8c730dc6da424bc9beaa584daf9e17ce0
Lautadelajara <ptoledo952@gmail.com>
2026-09-11 13:06:24 -0300

    feat: navbar, footer, home y rutas

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,9 +1,20 @@
-import React from 'react'
+import initialProducts from "../data/productos";
+import ProductCard from "../components/ProductCard";
 
 const Home = () => {
   return (
-    <div>Home</div>
-  )
-}
+    <main className="container py-4">
+      <h1 className="text-center mb-4">Nuestros productos</h1>
 
-export default Home
+      <div className="row g-4">
+        {initialProducts.map((product) => (
+          <div className="col-12 col-md-6 col-lg-4" key={product.id}>
+            <ProductCard product={product} />
+          </div>
+        ))}
+      </div>
+    </main>
+  );
+};
+
+export default Home;
