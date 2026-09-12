fa0f8e380e666cbd70505ee4ae2be47247e46422
Alvaro Medina <168005749+alvaroMedina777@users.noreply.github.com>
2026-09-12 09:57:34 -0300

    termino con los estilos de search bar

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -3,13 +3,13 @@ import React from 'react'
 const SearchBar = ({ busqueda, setBusqueda }) => {
 
   return (
-    <div className="mb-6">
+    <div className="mb-6 max-w-lg mx-auto">
       <input
         type="text"
-        placeholder="Ingrese que tipo de bicicleta busca"
+        placeholder="Buscar por categoría"
         value={busqueda}
         onChange={(e) => setBusqueda(e.target.value)}
-        className="w-full border rounded-lg p-3"
+        className="w-full border rounded-lg p-3 shadow-sm focus:outline-none focus:ring-2 focus:ring-indigo-500 max-w-xl mx-auto"
       />
     </div>
   )
